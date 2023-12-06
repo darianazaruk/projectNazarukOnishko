25704caf94d9057d5cc1b83e2b44948a9ccf596d
Daria <darianazaruk03@gmail.com>
2023-12-06 14:25:41 +0100

    first commit

---
+++ src/main/java/org/example/SimpleVoter.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Klasa reprezentująca konkretnego użytkownika
+public class SimpleVoter implements Voter {
+    private String fullName;
+    private Scanner scanner;
+
+    public SimpleVoter(String fullName,Scanner scanner) {
+        this.fullName = fullName;
+        this.scanner = scanner;
+    }
+
+    @Override
+    public String getFullName() {
+        return fullName;
+    }
+
+    @Override
+    public boolean vote(VotingQuestion question) {
+        question.askQuestion();
+
+        String answer = scanner.nextLine();
+        while (!question.isValidAnswer(answer)) {
+            System.out.println("Niepoprawna odpowiedź. Wprowadź 'tak' lub 'nie'.");
+            answer = scanner.nextLine();
+        }
+
+        return answer.equalsIgnoreCase("tak");
+
+    }
+}
---
+++ src/main/java/org/example/VotingSystem.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Główna klasa programu
+public class VotingSystem {
+    public static void main(String[] args) {
+        // Pytania do głosowania
+        VotingQuestion[] questions = {
+                new YesNoQuestion("Czy uważasz studia magisterskie za obowiązkowe?"),
+                new YesNoQuestion("Czy uważasz, że edukacja powinna być bezpłatna?")
+        };
+
+        // Tworzenie systemu zapisu wyników do pliku
+        ResultWriter resultWriter = new TextFileResultWriter("wyniki.txt");
+
+        // Przykładowe głosowanie
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Witaj w systemie głosowania!");
+        System.out.println("Podaj swoje imię i nazwisko:");
+        String fullName = scanner.nextLine();
+
+        // Tworzenie użytkownika
+        Voter voter = new SimpleVoter(fullName,scanner);
+
+        // Głosowanie na każde pytanie
+        StringBuilder result = new StringBuilder(voter.getFullName()).append(": ");
+        for (VotingQuestion question : questions) {
+            boolean vote = voter.vote(question);
+            result.append(question.getQuestionText()).append("-").append(vote ? "tak" : "nie").append(", ");
+        }
+        // Zapis wyników do pliku
+        resultWriter.writeResult(result.toString());
+
+        System.out.println("Dziękujemy za udział w głosowaniu!");
+    }
+}
